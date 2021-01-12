9ea123935e97c26337b39535a50d2b7e8a1169a2
Thor Muto Asmund <thorasmund@gmail.com>
2021-01-12 01:37:18 +0100

    * Add auto reconnect
    * Add all notes off

--- KeyboardMidi/MainWindow.xaml.cs
+++ KeyboardMidi/MainWindow.xaml.cs
@@ -6,6 +6,11 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
+
+// References
+// https://github.com/obiwanjacobi/midi.net/tree/master/Source/Samples/CannedBytes.Midi.Samples.SysExUtil
+// https://users.cs.cf.ac.uk/Dave.Marshall/Multimedia/node158.html#:~:text=MIDI%20messages%20are%20used%20by%20MIDI%20devices%20to%20communicate%20with%20each%20other.&text=MIDI%20message%20includes%20a%20status,bits%20produce%2016%20possible%20channels).
 
 namespace KeyboardMidi
 {
@@ -17,10 +22,12 @@ namespace KeyboardMidi
         //private const string preferredOutput = "Teensy MIDI";
         
         private bool autoScroll = true;
-        private MidiOutPortCapsCollection midiOutCaps;
-        private MidiOutPort output = null;
+        private MidiOutPort midiOut = null;
         private List<Key> keysDown = new List<Key>();
         private byte octave = 5;
+        private DispatcherTimer timer;
+
+        private string PreferredOutput => Properties.Settings.Default.PreferredOutput;
 
         public MainWindow()
         {
@@ -29,86 +36,193 @@ namespace KeyboardMidi
             this.display.Text += "KeyboardMidi by Thor Muto Asmund (C) 2021\n";
             this.display.Text += "\n";
 
-            this.midiOutCaps = new MidiOutPortCapsCollection();
+            this.fileMenu.SubmenuOpened += (sender, e) => FileMenuSubmenuOpened();
+            this.closeOutputMenu.Click += (sender, e) => FileCloseOutput();
+            this.exitMenu.Click += (sender, e) => ExitApplication();
+            this.allNotesOffMenu.Click += (sender, e) => AllNotesOff();
+            // Try connect
+            OpenOutput(this.PreferredOutput);
 
-            var preferredOutput = Properties.Settings.Default.PreferredOutput;
-
-            foreach (var outCaps in midiOutCaps)
+            if (this.midiOut != null)
             {
-                this.display.Text += $"{midiOutCaps.IndexOf(outCaps)}: {outCaps.Name}\n";
-                var isPreferred = outCaps.Name.Equals(preferredOutput);
-                if (isPreferred)
-                {
-                    this.output = new MidiOutPort();
-                    output.Open(midiOutCaps.IndexOf(outCaps));
-                }
+                this.display.Text += $"Last used output device {this.PreferredOutput} opened\n";
+            }
+            else if (!String.IsNullOrEmpty(this.PreferredOutput))
+            {
+                this.display.Text += $"Waiting for output device to connect...\n";
+            }
+
+            // Start connect timer
+            this.timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1f);
+            timer.Tick += TryConnectPreferred;
+            timer.Start();
+        }
+
+        private void FileMenuSubmenuOpened()
+        {
+            this.outputsMenu.Items.Clear();
+            var menuMidiOutCaps = new MidiOutPortCapsCollection();
+
+            foreach (var outCaps in menuMidiOutCaps)
+            {
+                var isChecked = this.midiOut != null && this.midiOut.Capabilities.Name == outCaps.Name;
                 var item = new MenuItem()
                 {
                     Header = outCaps.Name,
                     IsCheckable = true,
-                    IsChecked = isPreferred
+                    IsChecked = isChecked
                 };
-                item.Click += (sender, e) => OpenOutput(midiOutCaps.IndexOf(outCaps), item);
+                item.Click += (sender, e) =>
+                {
+                    if (!OpenOutput(outCaps.Name))
+                    {
+                        MessageBox.Show($"Output {outCaps.Name} not available", "Output error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    }
+                };
                 this.outputsMenu.Items.Add(item);
             }
+            if (this.outputsMenu.Items.Count == 0)
+            {
+                var item = new MenuItem()
+                {
+                    Header = "None",
+                    IsCheckable = false,
+                    FontStyle = FontStyles.Italic
+                };
+                this.outputsMenu.Items.Add(item);
+            }
+        }
 
-            this.display.Text += output == null ? "Select output\n" : $"Preferred output {preferredOutput} selected\n";
+        private void TryConnectPreferred(object sender, EventArgs e)
+        {
+            if (this.midiOut != null)
+            {
+                var deviceDisconnected = false;
 
-            this.closeOutputMenu.Click += (sender, e) => CloseOutput();
-            this.exitMenu.Click += (sender, e) => System.Windows.Application.Current.Shutdown();
+                if (this.midiOut.Status == MidiPortStatus.Closed)
+                {
+                    deviceDisconnected = true;
+                }
+                else
+                {
+                    var menuMidiOutCaps = new MidiOutPortCapsCollection();
+
+                    // Accessing this.midiOut.Capabilities on a disconnected (but not yet closed) midiOut throws an exception
+                    try
+                    {
+                        if (this.midiOut.Capabilities == null || !menuMidiOutCaps.Any(oc => oc.Name == this.midiOut.Capabilities.Name))
+                        {
+                            deviceDisconnected = true;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        deviceDisconnected = true;
+                    }
+                }
+
+                if (deviceDisconnected)
+                {
+                    this.display.Text += "Device disconnected\n";
+                    this.midiOut = null;
+                }
+            }
+
+            OpenOutput(this.PreferredOutput);
+        }
+
+        private void FileCloseOutput()
+        {
+            Properties.Settings.Default.PreferredOutput = null;
+            Properties.Settings.Default.Save();
+
+            CloseOutput();
         }
 
         private void CloseOutput()
         {
-            if (this.output != null)
+            if (this.midiOut != null)
             {
-                this.output.Close();
-                this.output = null;
+                this.midiOut.Close();
+                this.midiOut = null;
             }
         }
 
-        private void OpenOutput(int index, MenuItem selectedItem)
+        private void ExitApplication()
         {
-            if (this.output != null)
-            {
-                this.output.Close();
-            }
-            this.output = new MidiOutPort();
-            output.Open(index);
+            CloseOutput();
+            Application.Current.Shutdown();
+        }
 
-            foreach (var item in this.outputsMenu.Items)
+        private void AllNotesOff()
+        {
+            if (this.midiOut != null)
             {
-                if (item is MenuItem menuItem)
+                for (int c = 0; c < 16; ++c)
                 {
-                    menuItem.IsChecked = item == selectedItem;
+                    for (int note = 0; note < 128; ++note)
+                    {
+                        var midiData = new MidiData()
+                        {
+                            Status = (byte)(128+c),
+                            Parameter1 = (byte)(note),
+                            Parameter2 = 0
+                        };
+
+                        this.midiOut.ShortData(midiData);
+                    }
                 }
             }
+        }
 
-            var name = midiOutCaps[index].Name;
+        private bool OpenOutput(string name)
+        {
+            var menuMidiOutCaps = new MidiOutPortCapsCollection();
+            var index = menuMidiOutCaps.IndexOf(menuMidiOutCaps.FirstOrDefault(oc => oc.Name == name));
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            CloseOutput();
+
+            this.midiOut = new MidiOutPort();
+            this.midiOut.Open(index);
+
             Properties.Settings.Default.PreferredOutput = name;
             Properties.Settings.Default.Save();
+
+            this.display.Text += $"New output {name} selected\n";
+
+            return true;
         }
 
         private void MainKeyDown(object sender, KeyEventArgs e)
         {
-            if (output != null)
+            if (this.midiOut != null)
             {
                 ReadMidiKey(e.Key, true);
             }
         }
         private void MainKeyUp(object sender, KeyEventArgs e)
         {
-            if (output != null)
+            if (this.midiOut != null)
             {
                 ReadMidiKey(e.Key, false);
             }
         }
 
-        private void ReadMidiKey(Key key, bool down)
+        private void ReadMidiKey(Key key, bool on)
         {
             var keyAlreadyPressed = this.keysDown.Contains(key);
 
-            if (down)
+            if (on)
             {
                 if (keyAlreadyPressed)
                 {
@@ -123,59 +237,59 @@ namespace KeyboardMidi
 
             switch (key)
             {
-                case Key.D1: SendNoteOn(59, down); break;
-                case Key.Q: SendNoteOn(60, down); break;
-                case Key.D2: SendNoteOn(61, down); break;
-                case Key.W: SendNoteOn(62, down); break;
-                case Key.D3: SendNoteOn(63, down); break;
-                case Key.E: SendNoteOn(64, down); break;
-                case Key.R: SendNoteOn(65, down); break;
-                case Key.D5: SendNoteOn(66, down); break;
-                case Key.T: SendNoteOn(67, down); break;
-                case Key.D6: SendNoteOn(68, down); break;
-                case Key.Y: SendNoteOn(69, down); break;
-                case Key.D7: SendNoteOn(70, down); break;
-                case Key.U: SendNoteOn(71, down); break;
-                case Key.I: SendNoteOn(72, down); break;
-                case Key.D9: SendNoteOn(73, down); break;
-                case Key.O: SendNoteOn(74, down); break;
-                case Key.D0: SendNoteOn(75, down); break;
-                case Key.P: SendNoteOn(76, down); break;
+                case Key.D1: SendNote(59, on); break;
+                case Key.Q: SendNote(60, on); break;
+                case Key.D2: SendNote(61, on); break;
+                case Key.W: SendNote(62, on); break;
+                case Key.D3: SendNote(63, on); break;
+                case Key.E: SendNote(64, on); break;
+                case Key.R: SendNote(65, on); break;
+                case Key.D5: SendNote(66, on); break;
+                case Key.T: SendNote(67, on); break;
+                case Key.D6: SendNote(68, on); break;
+                case Key.Y: SendNote(69, on); break;
+                case Key.D7: SendNote(70, on); break;
+                case Key.U: SendNote(71, on); break;
+                case Key.I: SendNote(72, on); break;
+                case Key.D9: SendNote(73, on); break;
+                case Key.O: SendNote(74, on); break;
+                case Key.D0: SendNote(75, on); break;
+                case Key.P: SendNote(76, on); break;
 
                 //case Key.D1: SendNoteOn(59, down); break;
-                case Key.Z: SendNoteOn(48, down); break;
-                case Key.S: SendNoteOn(49, down); break;
-                case Key.X: SendNoteOn(50, down); break;
-                case Key.D: SendNoteOn(51, down); break;
-                case Key.C: SendNoteOn(52, down); break;
-                case Key.V: SendNoteOn(53, down); break;
-                case Key.G: SendNoteOn(54, down); break;
-                case Key.B: SendNoteOn(55, down); break;
-                case Key.H: SendNoteOn(56, down); break;
-                case Key.N: SendNoteOn(57, down); break;
-                case Key.J: SendNoteOn(58, down); break;
-                case Key.M: SendNoteOn(59, down); break;
-                case Key.OemComma: SendNoteOn(60, down); break;
+                case Key.Z: SendNote(48, on); break;
+                case Key.S: SendNote(49, on); break;
+                case Key.X: SendNote(50, on); break;
+                case Key.D: SendNote(51, on); break;
+                case Key.C: SendNote(52, on); break;
+                case Key.V: SendNote(53, on); break;
+                case Key.G: SendNote(54, on); break;
+                case Key.B: SendNote(55, on); break;
+                case Key.H: SendNote(56, on); break;
+                case Key.N: SendNote(57, on); break;
+                case Key.J: SendNote(58, on); break;
+                case Key.M: SendNote(59, on); break;
+                case Key.OemComma: SendNote(60, on); break;
 
                 case Key.OemOpenBrackets: if (this.octave > 1) this.octave--; break;
                 case Key.OemCloseBrackets: if (this.octave < 7) this.octave++; break;
             }
         }
 
-        private void SendNoteOn(byte note, bool down, byte velocity = 0x70)
+        private void SendNote(byte note, bool on, byte velocity = 0x70)
         {
             try
             {
-                if (output != null)
+                if (this.midiOut != null)
                 {
                     var midiData = new MidiData()
                     {
-                        Status = down ? (byte)144 : (byte)128,
+                        Status = on ? (byte)144 : (byte)128,
                         Parameter1 = (byte)(note + (octave - 5) * 12),
                         Parameter2 = velocity
                     };
 
-                    this.output.ShortData(midiData);
+                    this.midiOut.ShortData(midiData);
                     this.display.Text += $"0x{midiData.Status:x} 0x{midiData.Parameter1:x} 0x{midiData.Parameter2:x}\n";
                 }
             }
@@ -190,23 +304,23 @@ namespace KeyboardMidi
             // User scroll event : set or unset auto-scroll mode
             if (e.ExtentHeightChange == 0)
             {   // Content unchanged : user scroll event
-                if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+                if (this.scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
                 {   // Scroll bar is in bottom
                     // Set auto-scroll mode
-                    autoScroll = true;
+                    this.autoScroll = true;
                 }
                 else
                 {   // Scroll bar isn't in bottom
                     // Unset auto-scroll mode
-                    autoScroll = false;
+                    this.autoScroll = false;
                 }
             }
 
             // Content scroll event : auto-scroll eventually
-            if (autoScroll && e.ExtentHeightChange != 0)
+            if (this.autoScroll && e.ExtentHeightChange != 0)
             {   // Content changed and auto-scroll mode set
                 // Autoscroll
-                scrollViewer.ScrollToVerticalOffset(scrollViewer.ExtentHeight);
+                this.scrollViewer.ScrollToVerticalOffset(scrollViewer.ExtentHeight);
             }
         }
     }
